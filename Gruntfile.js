246214338d99de492af5f3bdfb740161fb11e24b
jmargieh <J.margieh@gmail.com>
2015-03-25 11:16:34 +0200

    default task - watch & md2html
    Added watch task in order to watch md files and convert them to html on
    file change
    Default grunt task will start server & watch tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,22 +1,49 @@
 module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-      connect: {
-    server: {
-      options: {
-        port: 8080,
-        protocol: 'http',
-        base: '.',
-        hostname: 'localhost',
-        keepalive: true
+    connect: {
+    	server: {
+      		options: {
+        	port: 8080,
+        	protocol: 'http',
+        	base: '.',
+        	hostname: 'localhost',
+        	keepalive: false
+      		}
+    	}
+  	},
+  	md2html: {
+      one_file: {
+        options: {},
+        files: [{
+          src: ['README.md'],
+          dest: 'README.html'
+        }]
       }
-    }
-  }
+    },
+    watch: {
+  		scripts: {
+    	files: ['**/*.md'],
+    	tasks: ['md2html'],
+    	options: {
+      	spawn: false
+    	}
+  	}
+}
   });
 
 
 
+  	grunt.loadNpmTasks('grunt-md2html'); // load ms2html grunt task
 
-	grunt.loadNpmTasks('grunt-contrib-connect');
+  	grunt.loadNpmTasks('grunt-contrib-watch'); // load watch task
+
+	grunt.loadNpmTasks('grunt-contrib-connect'); // start server
+
+
+	grunt.registerTask('default', 'Default Grunt Task Runner', function() {
+  	grunt.task.run(['connect', 'md2html','watch']);
+	});
+
 
 };
